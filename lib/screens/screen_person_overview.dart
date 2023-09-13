c5fe481ee5a33a093c5c4e6e81e78b720ceba8fa
EmreTuran99 <designeremo@gmail.com>
2023-09-13 15:02:37 +0300

    Added delete person option to person overview screen

--- lib/screens/screen_person_overview.dart
+++ lib/screens/screen_person_overview.dart
@@ -1,10 +1,15 @@
 
+import 'package:birthday_app/database/birthdaysDB.dart';
 import 'package:birthday_app/models/person.dart';
 import 'package:birthday_app/utils/enums.dart';
 import 'package:birthday_app/utils/methods.dart';
 import 'package:birthday_app/utils/providers.dart';
+import 'package:birthday_app/utils/services/local_notif_service.dart';
 import 'package:birthday_app/utils/styling.dart';
 import 'package:birthday_app/utils/values.dart';
+import 'package:birthday_app/widgets/failure_snackbar.dart';
+import 'package:birthday_app/widgets/success_snackbar.dart';
+import 'package:birthday_app/widgets/vertical_space.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -56,11 +61,95 @@ class _PersonOverviewScreenState extends ConsumerState<PersonOverviewScreen> {
     );
   }
 
+  Future<void> deletePerson() async {
+
+    ScaffoldMessengerState messengerState = ScaffoldMessenger.of(context);
+    NavigatorState navigatorState = Navigator.of(context);
+    navigatorState.pop();
+
+    bool res = await BirthdaysDB.instance.deletePersonFromDB(thePerson.personID!);
+
+    if(res){      
+      LocalNotifService.instance.cancelScheduledNotification(thePerson.personID!);
+      ref.read(personNotifierProvider.notifier).removePersonFromList(thePerson.personID!);
+      messengerState..clearSnackBars()..showSnackBar(
+        SuccessSnackbar("Kişi silindi")
+      );
+      navigatorState.pop();
+    }
+    else{
+      messengerState..clearSnackBars()..showSnackBar(
+        FailureSnackbar("Bir hata meydana geldi")
+      );
+    }
+  }
+
+  void showOptionsBottomNavSheet() async {
+
+    await showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.white,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical( 
+          top: Radius.circular(25.0),
+        ),
+      ),
+      constraints: BoxConstraints(
+        maxHeight: screenSize.height * 0.25
+      ),
+      builder: (context) {
+        
+        return Column(
+          children: [
+            VerticalSpace(12),
+            ListTile(
+              horizontalTitleGap: 0,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+              onTap: null, // TODO: implement edit person
+              leading: const Icon(Icons.edit, color: Colors.black,),
+              title: Text(
+                "Düzenle", 
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black, 
+                  fontWeight: FontWeight.bold,
+                  fontFamily: TextFonts.nunitoSans.fontName
+                ),
+              ),
+              subtitle: null,
+            ),
+            ListTile(
+              horizontalTitleGap: 0,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+              onTap: () => deletePerson(),
+              leading: const Icon(Icons.delete_outline_rounded, color: Colors.red,),
+              title: Text(
+                "Sil", 
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: TextFonts.nunitoSans.fontName
+                ),
+              ),
+              subtitle: null,
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
     List<Person> people = ref.watch(personNotifierProvider);
-    thePerson = people.firstWhere((person) => person.personID == widget.person.personID);
+    try{
+      thePerson = people.firstWhere((person) => person.personID == widget.person.personID);
+    }
+    catch(exp){
+      return Container();
+    }
     isAvatarMale = thePerson.avatarPath.compareTo(maleAvatarPath) == 0;
 
     return Scaffold(
@@ -72,6 +161,21 @@ class _PersonOverviewScreenState extends ConsumerState<PersonOverviewScreen> {
             automaticallyImplyLeading: true,
             expandedHeight: screenSize.height * 0.25,
             leadingWidth: 64,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 6.0),
+                child: IconButton(
+                  onPressed: () {
+                    showOptionsBottomNavSheet();
+                  },
+                  icon: const Icon(
+                    Icons.more_vert_outlined,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ),
+              )
+            ],
             flexibleSpace: FlexibleSpaceBar(
               background: Container(
                 decoration: BoxDecoration(
